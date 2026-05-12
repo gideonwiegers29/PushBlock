d866494a364659080f0360b0c9c307a90657ca53
Tucker Vincent <tucker.vincent29@thorntonacademy.org>
2026-05-12 11:26:51 -0400

    Add level selection functionality and remove unused UIHandler script
    - Implemented mouse click detection for level selection in CameraSwitcher.cs, allowing players to select levels 1 through 7 based on mouse position.
    - Removed the UIHandler.cs script as it was empty and not utilized in the project.
    - Deleted associated meta file for UIHandler.cs.
    - Added a new solution file (Untitled.slnx) to organize project structure.

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -56,12 +56,77 @@ public class Background : MonoBehaviour
                 }
             }
         }
+        if (mouseY < 600)
+        {
+            if(mouseY > 370)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //level 1
+                    if(mouseX > 225)
+                    {
+                        if (mouseX < 450)
+                        {
+                            Debug.Log(1);
+                        }
+                    }
+                    //level 2
+                    if(mouseX > 540)
+                    {
+                        if (mouseX < 750)
+                        {
+                            Debug.Log(2);
+                        }
+                    }
+                    //level 3
+                    if(mouseX > 850)
+                    {
+                        if (mouseX < 1075)
+                        {
+                            Debug.Log(3);
+                        }
+                    }
+                    //level 4
+                    if(mouseX > 1150)
+                    {
+                        if (mouseX < 1400)
+                        {
+                            Debug.Log(4);
+                        }
+                    }
+                    //level 5
+                    if(mouseX > 1475)
+                    {
+                        if (mouseX < 1715)
+                        {
+                            Debug.Log(5);
+                        }
+                    } 
+                    // level 6
+                    if(mouseX > 1780)
+                    {
+                        if (mouseX < 2030)
+                        {
+                            Debug.Log(6);
+                        }
+                    }
+                    //level 7
+                    if(mouseX > 2100)
+                    {   
+                        if (mouseX < 2350)
+                        {
+                            Debug.Log(7);
+                        }
+                    }
+                }
+            }
+        }
         time += Time.deltaTime;
         frameCount++;   
         if (time >= pollingTime)
         {
             int frameRate = Mathf.RoundToInt(frameCount / time);
-            Debug.Log("FPS: " + frameRate);
+            // Debug.Log("FPS: " + frameRate);
 
             // Reset the counters
             time -= pollingTime;
